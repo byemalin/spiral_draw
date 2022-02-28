612e1d970683390687f118c3f7530bd38f21fe20
malin888 <mabim149@newschool.edu>
2022-02-28 17:18:30 -0500

    scrollfix

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,64 +5,31 @@ function setup() {
     let c =createCanvas(1500,900);
     background(color("#f2ede3"));
   
-  
   fillColorPicker = createColorPicker("#F4F1DE");
   fillColorPicker.position(30, 400);
-
-  
   strokeColorPicker = createColorPicker("#E07A5F");
   strokeColorPicker.position(30, 480);
 
-
-
-
   saveBtn = createButton("Save Drawing");
-//   removeBtn.position(30, 200)
   saveBtn.mousePressed(saveToFile);
-
   saveBtn.position(30, 560);
 
   clearBtn = createButton("Clear");
-//   removeBtn.position(30, 200)
   clearBtn.mousePressed(clearFile);
-
-  clearBtn.position(30,320);
-
-
- 
-
-  
-  
+  clearBtn.position(30,320); 
 }
 
-
-
 function draw() {
-    
-  
   if (mouseIsPressed) {
-  
     translate(mouseX, mouseY);
     rotate(angle);
 
     fill(fillColorPicker.color());
     stroke(strokeColorPicker.color());
-    
-    
-    
     rect(-40, -40, 80, 80);
     angle += 0.1;
   }
-  
-  
 }
-
-// function keyPressed(){
-//     if (key == ' '){ 
-//     saveCanvas(c)
-//     console.log("PRESSED")
-//     }
-// }
 
 function saveToFile() {
     // Save the current canvas to file as png
